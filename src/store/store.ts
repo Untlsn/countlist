ba5b026924327fe350ff54a14c893ffd07cec116
Untel <Untellson@gmail.com>
2021-06-14 11:39:40 +0200

    Rebase

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
 import { combineReducers, createStore } from 'redux';
 import { mini, lists } from './ducks';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducers = combineReducers({
   mini,
   lists,
 });
 
-const store = createStore(reducers, composeWithDevTools());
+const store = createStore(
+  reducers,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
 
