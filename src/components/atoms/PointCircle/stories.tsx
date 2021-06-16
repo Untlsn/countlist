53039eadd82c1a83f4929c2f1c557d98804aad8f
Untel <Untellson@gmail.com>
2021-06-16 16:07:13 +0200

    Add PointCircle

---
+++ src/components/atoms/PointCircle/stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import PointCircle, { PointCircleProps }  from './index';
+import { useBoolState } from '@hooks';
+
+export default {
+  title: 'Atoms/PointCircle',
+} as Meta;
+
+
+const Template: Story<PointCircleProps> = (props) => {
+  const [checked, changeChecked] = useBoolState(props.checked);
+
+  return <PointCircle checked={checked} onClick={changeChecked} />;
+};
+
+export const Default = Template.bind({});
+Default.args = { checked: false };
+
+export const Checked = Template.bind({});
+Checked.args = { checked: true };
